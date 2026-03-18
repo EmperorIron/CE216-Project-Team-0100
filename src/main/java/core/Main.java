9c6f46e974505462bdbcc4d592fbd268ab50f5ae
EmperorIron <154456782+EmperorIron@users.noreply.github.com>
2026-03-18 23:21:08 +0300

    Fixes and implementation of javafx
    just start

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,7 +1,7 @@
-package ce216.team0100;
+package core;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, World!");
     }
 }
