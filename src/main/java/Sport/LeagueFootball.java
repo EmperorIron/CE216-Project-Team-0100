515c1c214ece26af4d6afccad30bccd01f555ac5
Cagritasoz <cagritasoz@gmail.com>
2026-04-08 21:50:50 +0300

    Calender, Game, League implementations for football sport.

---
+++ src/main/java/Sport/LeagueFootball.java
@@ -0,0 +1,32 @@
+package Sport;
+import Classes.*;
+import Sport.*;
+
+import Interface.ITeam;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
+
+public class LeagueFootball extends League {
+
+    public LeagueFootball(String name, String country, int numberOfTeams, GameRulesFootball rules) {
+        super(name, country);
+        for (int i = 0; i < numberOfTeams; i++) {
+            TeamFootball team = TeamGeneratorFootball.createRandomFootballTeam(rules);
+            this.addTeam(team);
+        }
+    }
+
+    @Override
+    public List<ITeam> getTeamRanking() {
+        List<ITeam> rankedTeams = new ArrayList<>(this.teams);
+        rankedTeams.sort(Comparator
+                .comparingInt(ITeam::getPoints)
+                .thenComparingInt(ITeam::getGoalDifference)
+                .thenComparingInt(ITeam::getGoalsScored)
+                .reversed()); 
+        return rankedTeams;
+    }
+}
